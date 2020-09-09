34149e0b43bcbe61411996818c3cc33c5fdd5001
Simon Peppitt <simonworkpersonal@gmail.com>
2020-09-09 17:00:21 +0100

    added action and cleaned out runner

--- src/main/java/com/qa/main/Runner.java
+++ src/main/java/com/qa/main/Runner.java
@@ -1,45 +1,18 @@
 package com.qa.main;
 
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class Runner {
 	
-	private static Scanner scan = new Scanner(System.in);
 
-	public static void main(String[] args) throws SQLException {
-
-		DatabaseConnector db = new DatabaseConnector();
-		String action = "";
+	public static void main(String[] args)  {
 		
-		try {	
-			do {
-				action = CRUDer.getCRUD();
-				switch (action) {
-				case "C":
-					CRUDer.create();
-					break;
-				case "R":
-					CRUDer.read();
-					break;
-				case "U":
-					CRUDer.update();
-					break;
-				case "D":
-					CRUDer.delete();
-					break;
-				case "E":
-					break;
-				default:
-					System.out.println("No Match");
-				}
-			}
-			while (!action.equals("E"));
-			System.out.println("Exiting");
-		}
-		finally {
-			scan.close();
-			db.close();
+		try {
+			Action.getAction();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
+
+
